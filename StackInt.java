0b5a950d424c22fe17e560451e5bc6984378af35
jcampolim <121445600+jcampolim@users.noreply.github.com>
2023-11-24 16:01:07 -0300

    Add files via upload

---
+++ StackInt.java
@@ -0,0 +1,85 @@
+package Pilha;
+
+public class StackInt {
+	
+	private int count;
+	private int tam;
+	private int stack[];
+	
+	private static final int MAX = 20;
+	
+	public StackInt(int tam) {
+		this.count = -1;
+		this.setTam(tam);
+		stack = new int[tam];
+	}
+	
+	public StackInt() {
+		this(MAX);
+	}
+	
+	public int pop() {
+		if(isEmpty() == true) {
+			System.out.println("Stack Underflow");
+			return 0;
+		} else {
+			int top = stack[count];
+			count--;
+			return top;
+		}
+	}
+	
+	public void push(int value) {
+		if(isFull() == true) {
+			System.out.println("Stack Overflow");
+		} else {
+			stack[++count] = value;
+		}
+	}
+	
+	public int top() {
+		if(isEmpty() == true) {
+			System.out.println("Stack Underflow");
+			return 0;
+		} else {
+			return stack[count];
+		}
+	}
+	
+	public int size() {
+		return count + 1;
+	}
+	
+	public boolean isEmpty() {
+		if(count == -1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean isFull() {
+		if(count == stack.length) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public void clear() {
+		for(int i = 0; i < size(); i++) {
+			pop();
+		}
+		
+		count = 0;
+	}
+
+	public int getTam() {
+		return tam;
+	}
+
+	public void setTam(int tam) {
+		this.tam = tam;
+	}
+
+}
